fa9ba0e1209987851f2e52e90822b3c77dcdebbc
acasune <tttt4aaaa@gmail.com>
2023-07-15 16:25:27 +0900

    add 2.5 and its test

---
+++ src/event_queue.rs
@@ -0,0 +1,24 @@
+use crate::event::Event;
+use std::collections::VecDeque;
+
+#[derive(Debug, Clone, Hash, PartialEq)]
+pub struct EventQueue(VecDeque<Event>);
+
+impl EventQueue {
+    pub fn new() -> Self {
+        EventQueue(VecDeque::new())
+    }
+    pub fn enqueue(&mut self, event: Event) {
+        self.0.push_front(event)
+    }
+
+    pub fn dequeue(&mut self) -> Option<Event> {
+        self.0.pop_back()
+    }
+}
+
+#[derive(PartialEq, Eq, Debug, Clone, Copy, Hash)]
+pub enum State {
+    Idle,
+    Connect,
+}
