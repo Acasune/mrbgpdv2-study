01bc15fa329079620b03d77d7476c6b212e52968
acasune <tttt4aaaa@gmail.com>
2023-07-17 15:09:08 +0900

    fix for unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,25 @@
-use mrbgpdv2::config::Config;
-use mrbgpdv2::peer::Peer;
+use std::env;
 use std::str::FromStr;
 
-use tokio::time::{sleep, Duration};
+use mrbgpdv2::config::Config;
+use mrbgpdv2::peer::Peer;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
-    let configs = vec![Config::from_str("64512 127.0.0.1 65413 127.0.0.2 active").unwrap()];
+    let config = env::args().skip(1).fold("".to_owned(), |mut acc, s| {
+        acc += &(s.to_owned() + " ");
+        acc
+    });
+    let config = config.trim_end();
+    let configs = vec![Config::from_str(&config).unwrap()];
     let mut peers: Vec<Peer> = configs.into_iter().map(Peer::new).collect();
     for peer in &mut peers {
         peer.start();
     }
 
-    let mut handles = vec![];
-    for mut peer in peers {
-        let handle = tokio::spawn(async move {
-            loop {
-                peer.next().await;
-                sleep(Duration::from_secs_f32(0.1)).await;
-            }
-        });
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        handle.await;
+    loop {
+        for peer in &mut peers {
+            peer.next().await;
+        }
     }
 }
